172211c68f6b773df61e25bcd6d3c8391f1a1cf2
joey shin <joeyshin613@gmail.com>
2022-10-05 10:37:48 -0400

    Smart Contract

---
+++ parautility/src/contracts/Master.sol
@@ -0,0 +1,138 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.0;
+
+// import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Master is Ownable {
+    using SafeMath for uint;
+
+    struct Customer {
+        // 0 level means doesn't exist
+        mapping(Restaurant => uint) public membersOfRestaurants;
+    }
+
+    struct Restaurant {
+        uint maxLevels;
+        uint percentDiscount;
+        uint totalRaised;
+        uint totalPending;
+        bool approved;
+
+    }
+
+    mapping(address => Restaurant) public restaurants;
+    mapping(address => mapping(address=> Customer)) public customers;
+
+    //----------Events--------------
+
+    event NewRestaurant(address restaurant);
+    event Withdrawl(address restaurant, uint amount);
+
+
+
+    //----------Modifiers-----------
+    modifier notZeroAddress(address _address) {
+        require(_address != address(0), "CANNOT be Zero Address");
+        _;
+    }
+
+    modifier notExistingRestaurant(address _restaurant) {
+        require(!restaurants[_restaurant].approved, "CANNOT be approved Restaurant");
+        _;
+    }
+
+    modifier isExistingRestaurant(address _restaurant) {
+        require(restaurants[_restaurant].approved, "Must be approved Restaurant");
+        _;
+    }
+
+    modifier addressesNotEqual(address _add1, address _add2) {
+        require(_add1 != _add2, "Addresses CANNOT be the same");
+        _;
+    }
+
+    //-------external functions ---
+
+    function renounceOwnership() public virtual onlyOwner override {
+        require(false, "Owner CANNOT be renounced");
+    }
+
+    function addRestaurant(address _restaurant)
+        external
+        onlyOwner
+        notZeroAddress(_restaurant)
+        notExistingCharity(_restaurant)
+        {
+        _addRestaurant(_restaurant);
+    }
+
+    function withdraw()
+        external
+        isExistingRestaurant(msg.sender)
+        {
+        _withdraw(msg.sender);
+    }
+
+    function saveFunds(address _restaurant)
+        external
+        onlyOwner
+        isExistingRestaurant(_restaurant)
+        {
+        _withdraw(_restaurant );
+    }
+
+    function _mintRestaurant(address _restaurant)
+        external payable
+        isExistingRestaurant(_restaurant)
+        notExistingRestaurant(msg.sender)
+        {
+        uint amount = msg.value;
+        require(amount > 0, "Donation CANNOT be Zero");
+        _mintRestaurant(_restaurant, amount);
+    }
+
+    //-------internal functions ---
+    function _addRestaurant(address _restaurant, uint percent, uint level) internal {
+        Restaurant storage restaurant = restaurants[_restaurant];
+        restaurant.maxLevels = level;
+        restaurant.percentDiscount = percent;
+        restaurant.approved = true;
+        restaurant.totalPending = 0;
+        restaurant.totalRaised = 0;
+        emit NewRestaurant(_restaurant);
+    }
+
+    function _withdraw(address _restaurant) internal {
+        Restaurant storage restaurant = restaurants[_restaurant];
+        uint amount = restaurant.totalPending;
+        require(amount > 0, "Pending CANNOT be Zero");
+        restaurant.totalPending = 0;
+        payable(_restaurant).transfer(amount);
+        emit Withdrawl(_restaurant, amount);
+    }
+    
+    function _mintRestaurant(address _restaurant, uint _amount) internal {
+        address sender = msg.sender;
+        Restaurant storage restaurant = restaurant[_restaurant];
+        Customer storage customer = customers[_restaurant][sender];
+        customer.membersOfRestaurants[restaurant] = _amount;
+        charity.totalRaised = charity.totalRaised.add(_amount);
+        charity.totalPending = charity.totalPending.add(_amount);
+    }
+
+
+    //-------getters functions ---
+    function getGiven(address _restaurant, address _customer) external view returns (uint amount) {
+       return customers[_restaurant][_customer].given;
+    }
+
+    function getRaised(address _restaurant, address _customer) external view returns (uint amount) {
+       return customers[_restaurant][_customer].raised;
+    }
+
+    function getTotalRaised(address _restaurant) external view returns (uint amount) {
+       return restaurant[_restaurant].totalRaised;
+    }
+}
---
+++ parautility/src/contracts/Restaurant.sol
@@ -0,0 +1,168 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract IDonations {
+    function getGiven(address _restaurant, address _customer) external view returns (uint){}
+    function getRaised(address _restaurant, address _customer) external view returns (uint){}
+}
+
+contract Restaurant is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+    using SafeMath for uint256;
+
+
+    IDonations controller;
+    bool public paused = false;
+    string public myContractURI;
+    address public charity;
+
+    struct IndividualNft {
+        uint minGiven;
+        uint maxGiven;
+        uint minRaised;
+        uint maxRaised;
+        string link;
+    }
+
+    mapping(address => uint) public userTokens;  //address --> their nft
+    mapping(uint => IndividualNft) public potentialNfts; //(1 -25) --> token info
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _myContractURI,
+        address _controller,
+        address _charity)
+        ERC721(_name, _symbol)
+        {
+        myContractURI = _myContractURI;
+        charity = _charity;
+        controller = IDonations(_controller);
+        _makeAllNFTs();
+    }
+
+    //----------Modifiers-----------
+    modifier inRange(uint requested) {
+        require(requested >= 1 && requested <= 25, "Token not in range");
+        _;
+    }
+
+    modifier qualifies(uint requested) {
+        uint given = controller.getGiven(charity, msg.sender);
+        uint raised = controller.getRaised(charity, msg.sender);
+        IndividualNft memory nft = potentialNfts[requested];
+        bool isQualified = (given != 0 || raised != 0) &&
+                           (given >= nft.minGiven) &&
+                           (given < nft.maxGiven) &&
+                           (raised >= nft.minRaised) &&
+                           (raised < nft.maxRaised);
+        require(isQualified, "Not qualified for this NFT");
+        _;
+    }
+
+    modifier isNotPaused() {
+        require(!paused, "The contract is paused");
+        _;
+    }
+
+    modifier doesntHaveNFT() {
+        require(userTokens[msg.sender] == 0, "You already have an NFT");
+        _;
+    }
+
+    modifier hasNFT() {
+        require(userTokens[msg.sender] != 0, "You need an NFT");
+        _;
+    }
+
+    //------external functions --------
+    function mint(uint requested)
+        external
+        inRange(requested)
+        isNotPaused
+        doesntHaveNFT
+        qualifies(requested)
+        {
+        //mint nft
+        uint256 supply = totalSupply();
+        _safeMint(msg.sender, supply.add(1));
+        userTokens[msg.sender] = requested;
+    }
+
+
+    function update(uint requested)
+        external
+        inRange(requested)
+        isNotPaused
+        hasNFT
+        qualifies(requested)
+        {
+        userTokens[msg.sender] = requested;
+    }
+
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        address nftOwner = ownerOf(tokenId);
+        uint nftIdentifier = userTokens[nftOwner];
+        string memory myTokenUri = potentialNfts[nftIdentifier].link;
+        return myTokenUri;
+    }
+
+    function contractURI() public view returns (string memory) {
+        return myContractURI;
+    }
+
+
+    function setContractURI(string memory _newContractURI) public onlyOwner {
+        myContractURI = _newContractURI;
+    }
+
+    function pause(bool _state) public onlyOwner {
+        paused = _state;
+    }
+
+    function withdraw() public payable onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+
+    // Make tokens untransferrable
+    function transferFrom(address from, address to, uint256 tokenId) public override {
+        require (false, "CANNOT Transfer");
+    }
+
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) public override {
+        require (false, "CANNOT Transfer");
+    }
+
+    //----- getters -----
+    function getTokenByUser(address _user) public view returns (uint) {
+        uint nftIdentifier = userTokens[_user];
+        return nftIdentifier;
+    }
+
+    //-------internal functions ---
+    function _makeNFT(
+        uint id,
+        uint _minGiven,
+        uint _maxGiven,
+        uint _minRaised,
+        uint _maxRaised,
+        string memory _link)
+        internal {
+        IndividualNft storage nft = potentialNfts[id];
+        nft.minGiven = _minGiven;
+        nft.maxGiven = _maxGiven;
+        nft.minRaised = _minRaised;
+        nft.maxRaised = _maxRaised;
+        nft.link = _link;
+
+    }
+}
