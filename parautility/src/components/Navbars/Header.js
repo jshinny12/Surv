6a746568697af3ba362ee4173c98a641d8ba6b3a
guarelin <guarelin@msu.edu>
2023-01-31 16:47:23 -0500

    Nav bar fix

--- parautility/src/components/Navbars/Header.js
+++ parautility/src/components/Navbars/Header.js
@@ -21,16 +21,15 @@ const Header = () => {
         <div styles= {{backgroundColor: '#181818', border: '1px solid black'}}>
         <Navbar collapseOnSelect bg="dark" fixed="top" expand='lg'>
             <Container>
-                <img src="./tradim/tradim_banner.png" height={40} align="left" href="/"/>
-            <Navbar.Brand href="/" style = {styles}>Tradim</Navbar.Brand>
+            <Navbar.Brand href="/" style = {styles}><img src="./tradim/tradim_banner.png" height={40} align="left" href="/"/></Navbar.Brand>
                 <Navbar.Brand href="/" style = {styles}>Welcome, {localStorage.getItem("fname")}</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsve-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="ml-auto">
+                <Nav className="me-auto" class="navbar-nav nav-fill w-100">
                     <Nav.Link href="/" style = {{color: 'white'}}>Home</Nav.Link>
                     {navBarRole === "merchant" ? <Nav.Link href="/my-company" style = {{color: 'white'}}>My Company</Nav.Link> : <></>}
                     {navBarRole === "customer" ? <Nav.Link href="/customer" style = {{color: 'white'}}>My Wallet</Nav.Link> : <></>}
-                    <!-- <Nav.Link href="/about" style = {{color: 'white'}} onClick = {console.log("hello")}>About</Nav.Link> -->
+                    {/* <Nav.Link href="/about" style={{color: 'white'}} onClick={console.log("hello")}>About</Nav.Link> */}
                     <Nav.Link href="/login" style = {{color: 'white'}}>Login</Nav.Link>
                     {navBarRole === "admin" ? <Nav.Link href="/users" style = {{color: 'white'}}>View User Table</Nav.Link> : <></>}
                     {navBarRole === "admin" ? <Nav.Link href="/admin-company-list" style = {{color: 'white'}}>View Company Table</Nav.Link> : <></>}
